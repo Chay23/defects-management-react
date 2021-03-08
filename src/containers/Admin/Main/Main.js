0eefd497a5bf2b69e4739e325102b9d0ae29fa32
Nazar Shcherbiy <Chay23@users.noreply.github.com>
2021-03-08 23:42:52 +0200

    Changed Main component

--- src/containers/Admin/Main/Main.js
+++ src/containers/Admin/Main/Main.js
@@ -41,6 +41,12 @@ class Main extends Component {
           ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum.
           Affert laboramus repudiandae nec et. Inciderint efficiantur his ad.
           Eum no molestiae voluptatibus.
+          <br />
+          Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum
+          definitiones no quo, maluisset concludaturque et eum, altera fabulas
+          ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum.
+          Affert laboramus repudiandae nec et. Inciderint efficiantur his ad.
+          Eum no molestiae voluptatibus.
         </h1>
       </div>
     );
